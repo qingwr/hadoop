d324441daf9bf13fffc27024e4b551644fe6d735
qingwr <864603923@qq.com>
2019-05-28 15:09:57 +0800

    wordCount demo

--- demo/wordCount/src/main/java/gz/dasijiaoyu/hadoop/WordCount.java
+++ demo/wordCount/src/main/java/gz/dasijiaoyu/hadoop/WordCount.java
@@ -1,8 +1,5 @@
 package gz.dasijiaoyu.hadoop;
 
-import java.io.IOException;
-import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -12,6 +9,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class WordCount {
 
@@ -52,7 +52,9 @@ public class WordCount {
         Job job = Job.getInstance(conf, "word count");
         job.setJarByClass(WordCount.class);
         job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
+        if(args.length > 2 && "combiner".equals(args[2])){
+            job.setCombinerClass(IntSumReducer.class);
+        }
         job.setReducerClass(IntSumReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
---
+++ demo/wordCount/src/main/java/gz/dasijiaoyu/hadoop/WordCountDefault.java
@@ -0,0 +1,29 @@
+package gz.dasijiaoyu.hadoop;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.map.TokenCounterMapper;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.reduce.IntSumReducer;
+
+import java.io.IOException;
+
+public class WordCountDefault {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+        Job job = new Job(conf, "word count2");
+        job.setJarByClass(WordCountDefault.class);
+        job.setMapperClass(TokenCounterMapper.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
