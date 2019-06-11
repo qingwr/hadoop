f613e0e77c07687668b342f4f7407d4c30e73090
hadoop <864603923@qq.com>
2019-06-10 23:08:15 -0700

    rpc server message

--- demo/rpc/src/main/java/com/dasijiaoyu/qwr/LoginServiceImpl.java
+++ demo/rpc/src/main/java/com/dasijiaoyu/qwr/LoginServiceImpl.java
@@ -3,6 +3,7 @@ package com.dasijiaoyu.qwr;
 public class LoginServiceImpl implements LoginServiceInterface {
     @Override
     public String login(String username, String password) {
+        System.out.println("logged username=" + username);
         return username + " logged in successfully ";
     }
 }
